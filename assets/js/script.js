bb2b95531e1ffcd093ecbfb7c0068ee675daac23
m4sterpaul18 <cirerachristian@gmail.com>
2021-04-11 12:11:35 +0800

    updated fonts

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('#fb-toggle').on('mouseleave mouseover',()=>{
+		$('#fb').slideToggle();
+	});	
+	$('#mail-toggle').on('mouseleave mouseover',()=>{
+		$('#mail').slideToggle();
+	});	
+	$('#msg-toggle').on('mouseleave mouseover',()=>{
+		$('#msg').slideToggle();
+	});	
+});
